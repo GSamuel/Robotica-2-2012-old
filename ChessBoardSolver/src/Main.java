700ba26834845d93d76c728a942fa2c40216be03
GSamuel <gideon_hoog@hotmail.com>
2012-09-21 17:47:52 +0200

    Pionnen weten nu naar welke posities ze kunnen, gegeven een bepaalde opstelling van pionnen op een bord
    Signed-off-by: GSamuel <gideon_hoog@hotmail.com>

--- ChessBoardSolver/src/Main.java
+++ ChessBoardSolver/src/Main.java
@@ -10,7 +10,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		Board board = new Board();
-		System.out.println(board.pieceOnPos(1,0));
+		System.out.println(board.toString());
 
 	}
 
--- ChessBoardSolver/src/com/gshoogeveen/Chess/pieces/Pawn.java
+++ ChessBoardSolver/src/com/gshoogeveen/Chess/pieces/Pawn.java
@@ -7,16 +7,53 @@ import com.gshoogeveen.Chess.Board.Board;
 
 public class Pawn extends Piece
 {
-	public Pawn(Team team, int x, int y)
+	public Pawn(Team team, Point pos)
 	{
-		super(team, x, y);
+		super(team, pos);
 	}
 
 	@Override
 	public ArrayList<Point> getAllMoves(Board board)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Point> points = new ArrayList<Point>();
+		int tempY,tempX, devY;
+		
+		if(team == Team.BLACK)
+			devY = -1;
+		else
+			devY = 1;
+		tempY = pos.y + devY;
+		
+		if(!board.pieceOnPos(pos.x, tempY) && board.onBoard(pos.x, tempY))
+		{
+			points.add(new Point(pos.x, tempY));
+			if(!this.hasMoved())
+			{
+				tempY = pos.y + devY*2;
+				if(!board.pieceOnPos(pos.x, tempY) && board.onBoard(pos.x, tempY))
+				{
+					points.add(new Point(pos.x, tempY));
+				}
+			}
+			
+		}
+
+		tempY = pos.y + devY;
+		tempX = pos.x - 1;
+		if(board.pieceOnPos(tempX,tempY) && board.getPieceOnPos(tempX,tempY).isOtherTeam(team))
+			points.add(new Point(tempX, tempY)); 
+
+		tempX = pos.x + 1;
+		if(board.pieceOnPos(tempX,tempY) && board.getPieceOnPos(tempX,tempY).isOtherTeam(team))
+			points.add(new Point(tempX, tempY));
+		
+		return points;
+	}
+
+	
+	public String toString()
+	{
+		return "Pawn "+ team.toString()+" "+pos.x+" "+pos.y;
 	}
 
 }
